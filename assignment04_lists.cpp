3f0c8f7602d08154452c7f74a11b6fd93ab221fe
Reid Amrhein <reidamrhein@gmail.com>
2026-03-22 20:26:43 -0400

    fixed some minor spacing issues

--- assignment04_lists.cpp
+++ assignment04_lists.cpp
@@ -304,12 +304,12 @@ template <typename T> class List{
         // Question 7
         // if list is empty
         if (this->isEmpty()) {
-          cout << "linked list is empty !" <<endl;
+          cout << "linked list is empty!" <<endl;
           return res;
         }
         // checks for index out of bounds
         if (index < 0 || index >= this->size()) {
-          cout<<"index out of bound !"<<endl;
+          cout << "index out of bound!" <<endl;
           return res;
         }
 
